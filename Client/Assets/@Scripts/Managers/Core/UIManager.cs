38fdd0865b7d46d83fdca3447b5583f4a59f6baa
DESKTOP-Q8290FB\Major <majorgking@gmail.com>
2025-06-23 22:46:22 +0900

    클라이언트 게임화면 및 UI해상도 1920*1080으로 수정

--- Client/Assets/@Scripts/Managers/Core/UIManager.cs
+++ Client/Assets/@Scripts/Managers/Core/UIManager.cs
@@ -60,7 +60,7 @@ public class UIManager
         if (cs != null)
         {
             cs.uiScaleMode = CanvasScaler.ScaleMode.ScaleWithScreenSize;
-            cs.referenceResolution = new Vector2(2640, 1080);
+            cs.referenceResolution = new Vector2(1920, 1080);
         }
 
         go.GetOrAddComponent<GraphicRaycaster>();
--- Client/Assets/@Scripts/Scenes/TitleScene.cs
+++ Client/Assets/@Scripts/Scenes/TitleScene.cs
@@ -23,6 +23,8 @@ public class TitleScene : BaseScene
 
         // 예외적으로 직접 등록한다 (UI_TitleScene은 애셋 로딩도 담당하기 때문)
         Managers.UI.SceneUI = GameObject.FindAnyObjectByType<UI_TitleScene>();
+
+        SetResolution();
     }
 
 	protected override void Start()
@@ -40,15 +42,37 @@ public class TitleScene : BaseScene
 
 	}
 
-	//IEnumerator CoSendTestPackets()
-	//{
-	//	while (true)
-	//	{
-	//		yield return new WaitForSeconds(1);
+    //IEnumerator CoSendTestPackets()
+    //{
+    //	while (true)
+    //	{
+    //		yield return new WaitForSeconds(1);
 
-	//		C_Test pkt = new C_Test();
-	//		pkt.Temp = 1;
-	//		Managers.Network.Send(pkt);
-	//	}
-	//}
+    //		C_Test pkt = new C_Test();
+    //		pkt.Temp = 1;
+    //		Managers.Network.Send(pkt);
+    //	}
+    //}
+
+    public void SetResolution()
+    {
+        int setWidth = 1920; // 사용자 설정 너비
+        int setHeight = 1080; // 사용자 설정 높이
+
+        int deviceWidth = Screen.width; // 기기 너비 저장
+        int deviceHeight = Screen.height; // 기기 높이 저장
+
+        Screen.SetResolution(setWidth, (int)(((float)deviceHeight / deviceWidth) * setWidth), true); // SetResolution 함수 제대로 사용하기
+
+        if ((float)setWidth / setHeight < (float)deviceWidth / deviceHeight) // 기기의 해상도 비가 더 큰 경우
+        {
+            float newWidth = ((float)setWidth / setHeight) / ((float)deviceWidth / deviceHeight); // 새로운 너비
+            Camera.main.rect = new Rect((1f - newWidth) / 2f, 0f, newWidth, 1f); // 새로운 Rect 적용
+        }
+        else // 게임의 해상도 비가 더 큰 경우
+        {
+            float newHeight = ((float)deviceWidth / deviceHeight) / ((float)setWidth / setHeight); // 새로운 높이
+            Camera.main.rect = new Rect(0f, (1f - newHeight) / 2f, 1f, newHeight); // 새로운 Rect 적용
+        }
+    }
 }
